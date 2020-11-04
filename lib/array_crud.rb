4c460029af15bf05fd74b36ee69d2f5405cd5618
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-04 21:24:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  
 end
